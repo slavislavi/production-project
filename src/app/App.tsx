c19e47f5d42600c80602a2cce9d7f5784360fcb4
slavislavi <slavislavi1986@gmail.com>
2023-02-02 15:27:14 +0300

    [main]: fsd-architecture-refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
-import "./styles/index.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import "app/styles/index.scss";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -16,8 +16,8 @@ export const App = () => {
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Загрузка...</div>}>
         <Routes>
-          <Route path="/about" element={<AboutPageAsync />} />
-          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
