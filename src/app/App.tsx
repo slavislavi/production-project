f3957cd4213f6604e89d0e487fe93532f21dd70f
slavislavi <slavislavi1986@gmail.com>
2023-02-06 22:41:15 +0300

    [main]: router-navbar-applink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,17 @@
-import { Suspense } from "react";
-import { Link, Route, Routes } from "react-router-dom";
-import { classNames } from "shared/lib/classNames/classNames";
+import { AppRouter } from "app/providers/router";
 import { useTheme } from "app/providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
 import "app/styles/index.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Navbar } from "widgets/Navbar";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
+      <Navbar />
+      <AppRouter />
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
-      <Suspense fallback={<div>Загрузка...</div>}>
-        <Routes>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </Suspense>
     </div>
   );
 };
