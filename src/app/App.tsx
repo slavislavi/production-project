edd06cdb0de7989d5db7c1a92e34d721f1d6e383
slavislavi <slavislavi1986@gmail.com>
2023-02-07 12:32:05 +0300

    [main]: theme-switcher-layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,13 +5,12 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { Navbar } from "widgets/Navbar";
 
 export const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
       <AppRouter />
-      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,15 +1,16 @@
-import { FC } from "react";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Navbar.module.scss";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string;
 }
 
-export const Navbar: FC<NavbarProps> = ({ className }) => {
+export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
+      <ThemeSwitcher />
       <div className={cls.links}>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
