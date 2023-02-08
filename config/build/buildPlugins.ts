ed7df99a9db4c8d6dd64ced1ab607dee11d504fe
slavislavi <slavislavi1986@gmail.com>
2023-02-08 17:41:26 +0300

    [main]: install-babel-utils

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,12 @@
-import { DefinePlugin, ProgressPlugin, WebpackPluginInstance } from "webpack";
+import {
+  DefinePlugin,
+  HotModuleReplacementPlugin,
+  ProgressPlugin,
+  WebpackPluginInstance,
+} from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import { BuildOptions } from "./types/config";
 
 export function buildPlugins({
@@ -19,5 +25,7 @@ export function buildPlugins({
     new DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
+    new HotModuleReplacementPlugin(),
+    new ReactRefreshWebpackPlugin(),
   ];
 }
