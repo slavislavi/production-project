409e4eacfd0470a4d5cf023b99811c8216f208bc
slavislavi <slavislavi1986@gmail.com>
2023-02-15 21:26:17 +0300

    [main]: setup-storybook-troubles

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,6 +1,7 @@
 import { addDecorator } from '@storybook/react';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
 import { Theme } from '../../src/app/providers/ThemeProvider';
 
 export const parameters = {
--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { Configuration } from 'webpack';
+import { Configuration, RuleSetRule } from 'webpack';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
 
@@ -10,8 +10,22 @@ export default ({ config }: { config: Configuration }) => {
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
-    config.resolve?.modules?.push(paths.src);
-    config.resolve?.extensions?.push('.ts', '.tsx');
+    config.resolve.modules.push(paths.src);
+    config.resolve.extensions.push('.ts', '.tsx');
+
+    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+        if (rule.test instanceof RegExp && rule.test.toString().includes('svg')) {
+            return { ...rule, exclude: /\.svg$/i };
+        }
+        return rule;
+    });
+
+    config.module?.rules?.push(
+        {
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        },
+    );
     config.module?.rules?.push(buildCssLoader(true));
 
     return config;
