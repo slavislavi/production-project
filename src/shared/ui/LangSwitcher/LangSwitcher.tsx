85d1b228c38dea0eaef64771f0b3d932bc2626d2
slavislavi <slavislavi1986@gmail.com>
2023-02-18 15:58:59 +0300

    [main]: langswitcher-on-collapse

--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -4,9 +4,10 @@ import { Button, ButtonVariant } from '../Button/Button';
 
 interface LangSwitcherProps {
   className?: string;
+  short?: boolean;
 }
 
-export const LangSwitcher = ({ className }: LangSwitcherProps) => {
+export const LangSwitcher = ({ className, short }: LangSwitcherProps) => {
     const { t, i18n } = useTranslation();
 
     const toggle = () => i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
@@ -17,7 +18,7 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
             variant={ButtonVariant.CLEAR}
             onClick={toggle}
         >
-            {t('Язык')}
+            {t(short ? 'Язык кратко' : 'Язык')}
         </Button>
     );
 };
