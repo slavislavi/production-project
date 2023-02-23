2d944a6a31cab8166fc3104fecc19238d26cfd17
slavislavi <slavislavi1986@gmail.com>
2023-02-23 14:12:02 +0300

    [main]: loginForm-loginModal-ui

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonVariant } from 'shared/ui/Button/Button';
-import { Modal } from 'shared/ui/Modal/Modal';
+import { LoginModal } from 'features/AuthByUsername';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -13,8 +13,12 @@ export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
     const [isAuthModal, setIsAuthModal] = useState(false);
 
-    const onToggleModal = useCallback(() => {
-        setIsAuthModal((prev) => !prev);
+    const onCloseModal = useCallback(() => {
+        setIsAuthModal(false);
+    }, []);
+
+    const showModal = useCallback(() => {
+        setIsAuthModal(true);
     }, []);
 
     return (
@@ -22,20 +26,11 @@ export const Navbar = ({ className }: NavbarProps) => {
             <Button
                 className={cls.links}
                 variant={ButtonVariant.CLEAR_INVERTED}
-                onClick={onToggleModal}
+                onClick={showModal}
             >
                 {t('Войти')}
             </Button>
-            <Modal
-                isOpen={isAuthModal}
-                onClose={onToggleModal}
-            // eslint-disable-next-line i18next/no-literal-string
-            >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Repudiandae, aspernatur iusto. Sit dicta voluptatem sunt doloremque quam!
-                Doloremque, eveniet! In enim sunt et fugiat velit ea tenetur
-                animi dicta molestias.
-            </Modal>
+            <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
         </div>
     );
 };
