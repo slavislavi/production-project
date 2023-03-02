8149519e89aa20dde3c7e9953b7b0aefdc29508a
slavislavi <slavislavi1986@gmail.com>
2023-03-02 18:29:30 +0300

    [main]: instance-api

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -10,21 +10,22 @@ interface LoginByUsernameProps {
 
 export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { rejectValue: string }>(
     'login/loginByUsername',
-    async (authData, thunkAPI) => {
+    async (authData, { dispatch, extra, rejectWithValue }) => {
         try {
-            const response = await axios.post('http://localhost:8000/login', authData);
+            // @ts-ignore
+            const response = await extra.api.post<User>('/login', authData);
 
             if (!response.data) {
                 throw new Error();
             }
 
             localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
-            thunkAPI.dispatch(userActions.setAuthData(response.data));
+            dispatch(userActions.setAuthData(response.data));
 
             return response.data;
         } catch (e) {
             console.log(e);
-            return thunkAPI.rejectWithValue('error');
+            return rejectWithValue('error');
         }
     },
 );
