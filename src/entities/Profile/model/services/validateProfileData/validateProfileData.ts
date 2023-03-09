442973cda49f5a611129b3fdac8deedabb17f67c
slavislavi <slavislavi1986@gmail.com>
2023-03-09 11:44:57 +0300

    [main]: add-validate-errors

---
+++ src/entities/Profile/model/services/validateProfileData/validateProfileData.ts
@@ -0,0 +1,32 @@
+import { Profile, ValidateProfileError } from '../../types/profile';
+
+export const validateProfileData = (profile?: Profile) => {
+    if (!profile) {
+        return [ValidateProfileError.NO_DATA];
+    }
+
+    const {
+        firstName,
+        lastName,
+        username,
+        age,
+        city,
+        country,
+    } = profile;
+
+    const errors: ValidateProfileError[] = [];
+
+    if (!firstName || !lastName || !username) {
+        errors.push(ValidateProfileError.INCORRECT_USER_DATA);
+    }
+
+    if (!age || !Number.isInteger(age)) {
+        errors.push(ValidateProfileError.INCORRECT_AGE_DATA);
+    }
+
+    if (!country || !city) {
+        errors.push(ValidateProfileError.INCORRECT_LOCATION_DATA);
+    }
+
+    return errors;
+};
--- src/entities/Profile/model/types/profile.ts
+++ src/entities/Profile/model/types/profile.ts
@@ -1,6 +1,14 @@
 import { Country } from 'entities/Country';
 import { Currency } from 'entities/Currency';
 
+export enum ValidateProfileError {
+    INCORRECT_USER_DATA = 'INCORRECT_USER_DATA',
+    INCORRECT_AGE_DATA = 'INCORRECT_AGE_DATA',
+    INCORRECT_LOCATION_DATA = 'INCORRECT_LOCATION_DATA',
+    NO_DATA = 'NO_DATA',
+    SERVER_ERROR = 'SERVER_ERROR',
+}
+
 export interface Profile {
     firstName?: string;
     lastName?: string;
@@ -18,4 +26,5 @@ export interface ProfileSchema {
     isLoading: boolean;
     error?: string;
     readonly: boolean;
+    validateErrors?: ValidateProfileError[];
 }
