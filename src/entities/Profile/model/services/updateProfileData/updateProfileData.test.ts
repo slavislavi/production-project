35e36fa894bff744776d4bb8610c0b3d2c4a92f1
slavislavi <slavislavi1986@gmail.com>
2023-03-09 20:25:28 +0300

    [main]: unit-tests-profile-services

---
+++ src/entities/Profile/model/services/updateProfileData/updateProfileData.test.ts
@@ -0,0 +1,60 @@
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+import { ValidateProfileError } from 'entities/Profile';
+import { updateProfileData } from './updateProfileData';
+
+const data = {
+    username: 'admin',
+    firstName: 'John',
+    lastName: 'Connor',
+    age: 24,
+    country: Country.US,
+    city: 'Chicago',
+    currency: Currency.USD,
+};
+
+describe('updateProfileData API service', () => {
+    test('success update', async () => {
+        const thunk = new TestAsyncThunk(updateProfileData, {
+            profile: {
+                form: data,
+            },
+        });
+        thunk.api.put.mockReturnValue(Promise.resolve({ data }));
+        const result = await thunk.callThunk();
+
+        expect(thunk.api.put).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe('fulfilled');
+        expect(result.payload).toEqual(data);
+    });
+
+    test('error update', async () => {
+        const thunk = new TestAsyncThunk(updateProfileData, {
+            profile: {
+                form: data,
+            },
+        });
+        thunk.api.put.mockReturnValue(Promise.resolve({ status: 403 }));
+        const result = await thunk.callThunk();
+
+        expect(result.meta.requestStatus).toBe('rejected');
+        expect(result.payload).toEqual([
+            ValidateProfileError.SERVER_ERROR,
+        ]);
+    });
+
+    test('validate error', async () => {
+        const thunk = new TestAsyncThunk(updateProfileData, {
+            profile: {
+                form: { ...data, lastName: '' },
+            },
+        });
+        const result = await thunk.callThunk();
+
+        expect(result.meta.requestStatus).toBe('rejected');
+        expect(result.payload).toEqual([
+            ValidateProfileError.INCORRECT_USER_DATA,
+        ]);
+    });
+});
---
+++ src/entities/Profile/model/services/validateProfileData/validateProfileData.test.ts
@@ -0,0 +1,54 @@
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+import { ValidateProfileError } from 'entities/Profile';
+import { validateProfileData } from './validateProfileData';
+
+const data = {
+    username: 'admin',
+    firstName: 'John',
+    lastName: 'Connor',
+    age: 24,
+    country: Country.US,
+    city: 'Chicago',
+    currency: Currency.USD,
+};
+
+describe('validateProfileData API service', () => {
+    test('success validate', async () => {
+        const result = validateProfileData(data);
+        expect(result).toEqual([]);
+    });
+
+    test('without firstName and lastName', async () => {
+        const result = validateProfileData({ ...data, firstName: '', lastName: '' });
+        expect(result).toEqual([
+            ValidateProfileError.INCORRECT_USER_DATA,
+        ]);
+    });
+
+    test('incorrect age', async () => {
+        const result = validateProfileData({ ...data, age: undefined });
+
+        expect(result).toEqual([
+            ValidateProfileError.INCORRECT_AGE_DATA,
+        ]);
+    });
+
+    test('incorrect country', async () => {
+        const result = validateProfileData({ ...data, country: undefined });
+
+        expect(result).toEqual([
+            ValidateProfileError.INCORRECT_LOCATION_DATA,
+        ]);
+    });
+
+    test('incorrect all', async () => {
+        const result = validateProfileData({});
+
+        expect(result).toEqual([
+            ValidateProfileError.INCORRECT_USER_DATA,
+            ValidateProfileError.INCORRECT_AGE_DATA,
+            ValidateProfileError.INCORRECT_LOCATION_DATA,
+        ]);
+    });
+});
