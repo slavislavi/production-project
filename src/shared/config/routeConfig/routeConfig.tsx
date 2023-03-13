56f212548bf4bb9811e73cd2bf09e339efb6feb7
slavislavi <slavislavi1986@gmail.com>
2023-03-13 16:38:26 +0300

    [main]: add-private-protected-routes

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -4,7 +4,7 @@ import { MainPage } from 'pages/MainPage';
 import { ProfilePage } from 'pages/ProfilePage';
 import { NotFoundPage } from 'pages/NotFoundPage';
 
-type AppRoutesProps = RouteProps & {
+export type AppRoutesProps = RouteProps & {
     authOnly?: boolean;
 }
 
