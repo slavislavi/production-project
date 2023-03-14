8771f6edd8ad2983e77cde58757961a64f21fafa
slavislavi <slavislavi1986@gmail.com>
2023-03-14 23:53:02 +0300

    [main]: article-details-ui-component-template

---
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -0,0 +1,18 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './ArticleDetails.module.scss';
+
+interface ArticleDetailsProps {
+    className?: string;
+}
+
+export const ArticleDetails = (props: ArticleDetailsProps) => {
+    const { className } = props;
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.articleDetails, {}, [className])}>
+            {/* children */}
+        </div>
+    );
+};
