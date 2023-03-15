76b2c709d7bf6d3262282648025610f156eac683
slavislavi <slavislavi1986@gmail.com>
2023-03-15 13:31:12 +0300

    [main]: article-details-selectors

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -1,18 +1,60 @@
+import { memo, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { fetchArticleById } from 'entities/Article/model/services/fetchArticleById/fetchArticleById';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { DynamicReducerLoader, ReducersList } from 'shared/lib/components/DynamicReducerLoader/DynamicReducerLoader';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { articleDetailsReducer } from '../../model/slice/articleDetailsSlice';
 import cls from './ArticleDetails.module.scss';
+import {
+    getArticleDetailsData,
+    getArticleDetailsError,
+    getArticleDetailsIsLoading,
+} from '../../model/selectors/articleDetails';
 
 interface ArticleDetailsProps {
     className?: string;
+    id: string;
 }
 
-export const ArticleDetails = (props: ArticleDetailsProps) => {
-    const { className } = props;
+const reducers: ReducersList = {
+    articleDetails: articleDetailsReducer,
+};
+
+export const ArticleDetails = memo((props: ArticleDetailsProps) => {
+    const { className, id } = props;
     const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+    const isLoading = useSelector(getArticleDetailsIsLoading);
+    const article = useSelector(getArticleDetailsData);
+    const error = useSelector(getArticleDetailsError);
+
+    useEffect(() => {
+        if (__PROJECT__ !== 'storybook') {
+            dispatch(fetchArticleById(id));
+        }
+    }, [dispatch, id]);
+
+    let content;
+
+    if (isLoading) {
+        content = (
+            <div>{isLoading}</div>
+        );
+    } else if (error) {
+        content = (
+            <div>{error}</div>
+        );
+    } else {
+        <div>{t('О статье', { ns: 'articleDetails' })}</div>;
+    }
 
     return (
-        <div className={classNames(cls.articleDetails, {}, [className])}>
-            {t('О статье', { ns: 'articleDetails' })}
-        </div>
+        <DynamicReducerLoader reducers={reducers} removeAfterUnmount>
+            <div className={classNames(cls.articleDetails, {}, [className])}>
+                {content}
+            </div>
+        </DynamicReducerLoader>
     );
-};
+});
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,3 +1,4 @@
+import { useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { ArticleDetails } from 'entities/Article';
 import { classNames } from 'shared/lib/classNames/classNames';
@@ -10,10 +11,19 @@ interface ArticleDetailsPageProps {
 export const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     const { className } = props;
     const { t } = useTranslation();
+    const { id } = useParams<{ id: string }>();
+
+    if (!id) {
+        return (
+            <div className={classNames(cls.articleDetailsPage, {}, [className])}>
+                {t('Статья не найдена', 'errors')}
+            </div>
+        );
+    }
 
     return (
         <div className={classNames(cls.articleDetailsPage, {}, [className])}>
-            <ArticleDetails />
+            <ArticleDetails id={id} />
         </div>
     );
 };
