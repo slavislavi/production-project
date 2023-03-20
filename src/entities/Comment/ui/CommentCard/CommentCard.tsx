cf67fc2173ca66786646eeff82692b45ecc90752
slavislavi <slavislavi1986@gmail.com>
2023-03-20 19:31:32 +0300

    [main]: loading-at-commentCard

--- src/entities/Comment/ui/CommentCard/CommentCard.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -9,7 +9,7 @@ import cls from './CommentCard.module.scss';
 
 interface CommentCardProps {
     className?: string;
-    comment: Comment;
+    comment?: Comment;
     isLoading?: boolean;
 }
 
@@ -18,7 +18,7 @@ export const CommentCard = (props: CommentCardProps) => {
 
     if (isLoading) {
         return (
-            <div className={classNames(cls.commentCard, {}, [className])}>
+            <div className={classNames(cls.commentCard, {}, [className, cls.loading])}>
                 <div className={cls.header}>
                     <Skeleton width={30} height={30} border="50%" />
                     <Skeleton width={100} height={16} className={cls.username} />
@@ -26,6 +26,10 @@ export const CommentCard = (props: CommentCardProps) => {
                 <Skeleton width="100%" height={50} className={cls.text} />
             </div>
         );
+    }
+
+    if (!comment) {
+        return null;
     }
 
     return (
