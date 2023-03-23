3009256125fd24c8fb051454e98b6f90f70af291
slavislavi <slavislavi1986@gmail.com>
2023-03-23 16:14:13 +0300

    [main]: articlesPage-selectors

--- src/pages/ArticlesPage/model/services/fetchArticlesList/fetchArticlesList.ts
+++ src/pages/ArticlesPage/model/services/fetchArticlesList/fetchArticlesList.ts
@@ -1,7 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from 'app/providers/StoreProvider';
 import { Article } from 'entities/Article';
-import { getArticlesPageLimit } from '../../model/selectors/articlesPageSelectors';
 
 interface FetchArticlesListProps {
     page?: number;
@@ -9,21 +8,17 @@ interface FetchArticlesListProps {
 
 export const fetchArticlesList = createAsyncThunk<
     Article[],
-    FetchArticlesListProps,
+    void,
     ThunkConfig<string>
     >(
         'articlesPage/fetchArticlesList',
         async (props, thunkApi) => {
             const { extra, rejectWithValue, getState } = thunkApi;
-            const { page = 1 } = props;
-            const limit = getArticlesPageLimit(getState());
 
             try {
                 const response = await extra.api.get<Article[]>('/articles', {
                     params: {
                         _expand: 'user',
-                        _limit: limit,
-                        _page: page,
                     },
                 });
 
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,9 +1,13 @@
 import { ArticleList, ArticleView } from 'entities/Article';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { DynamicReducerLoader, ReducersList } from 'shared/lib/components/DynamicReducerLoader/DynamicReducerLoader';
-import { articlesPageReducer } from '../../model/slice/articlesPageSlice';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
+import { articlesPageReducer, getArticles } from '../../model/slice/articlesPageSlice';
 import cls from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
@@ -17,6 +21,12 @@ const reducers: ReducersList = {
 export const ArticlesPage = memo((props: ArticlesPageProps) => {
     const { className } = props;
     const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+    const articles = useSelector(getArticles.selectAll);
+
+    useInitialEffect(() => {
+        dispatch(fetchArticlesList());
+    });
 
     return (
         <DynamicReducerLoader reducers={reducers}>
