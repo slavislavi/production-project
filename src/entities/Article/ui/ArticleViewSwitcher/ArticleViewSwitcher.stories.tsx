e13dae67630e5f841e235163900d4d72140d67e6
slavislavi <slavislavi1986@gmail.com>
2023-03-23 17:08:14 +0300

    [main]: article-view-swither-stories

--- src/entities/Article/ui/ArticleViewSwitcher/ArticleViewSwitcher.stories.tsx
+++ src/entities/Article/ui/ArticleViewSwitcher/ArticleViewSwitcher.stories.tsx
@@ -1,4 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ArticleView } from 'entities/Article';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { ArticleViewSwitcher } from './ArticleViewSwitcher';
 
 export default {
@@ -11,7 +14,24 @@ export default {
 
 const Template: ComponentStory<typeof ArticleViewSwitcher> = (args) => <ArticleViewSwitcher {...args} />;
 
-export const Normal = Template.bind({});
-Normal.args = {
-
+export const BigView = Template.bind({});
+BigView.args = {
+    view: ArticleView.BIG,
 };
+
+export const BigViewDark = Template.bind({});
+BigViewDark.args = {
+    view: ArticleView.BIG,
+};
+BigViewDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const SmallView = Template.bind({});
+SmallView.args = {
+    view: ArticleView.SMALL,
+};
+
+export const SmallViewDark = Template.bind({});
+SmallViewDark.args = {
+    view: ArticleView.SMALL,
+};
+SmallViewDark.decorators = [ThemeDecorator(Theme.DARK)];
