d30253e5d140a9f096dbf676dda6e7d64d665e71
slavislavi <slavislavi1986@gmail.com>
2023-04-04 23:31:23 +0300

    [main]: shared-text-semantic-refactor

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -30,6 +30,15 @@ interface TextProps {
     size?: TextSize;
 }
 
+type HeaderTagType = 'h1' | 'h2' | 'h3' | 'h4';
+
+const mapSizeToHeaderTag: Record<TextSize, HeaderTagType> = {
+    [TextSize.S]: 'h4',
+    [TextSize.M]: 'h3',
+    [TextSize.L]: 'h2',
+    [TextSize.XL]: 'h1',
+};
+
 export const Text = memo((props: TextProps) => {
     const {
         className,
@@ -40,6 +49,8 @@ export const Text = memo((props: TextProps) => {
         size = TextSize.M,
     } = props;
 
+    const HeaderTag = mapSizeToHeaderTag[size];
+
     const mods: Mods = {
         [cls[variant]]: true,
         [cls[align]]: true,
@@ -48,7 +59,7 @@ export const Text = memo((props: TextProps) => {
 
     return (
         <div className={classNames('', mods, [className])}>
-            {title && <p className={cls.title}>{title}</p>}
+            {title && <HeaderTag className={cls.title}>{title}</HeaderTag>}
             {text && <p className={cls.text}>{text}</p>}
         </div>
     );
