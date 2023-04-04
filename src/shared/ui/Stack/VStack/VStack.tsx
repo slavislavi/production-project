d7415eb6bb9babda56aad8170ca53073d362b180
slavislavi <slavislavi1986@gmail.com>
2023-04-04 20:29:41 +0300

    [main]: VStack-in-Sidebar

--- src/shared/ui/Stack/VStack/VStack.tsx
+++ src/shared/ui/Stack/VStack/VStack.tsx
@@ -2,6 +2,8 @@ import { Flex, FlexProps } from '../Flex/Flex';
 
 type VStackProps = Omit<FlexProps, 'direction'>;
 
-export const VStack = (props: VStackProps) => (
-    <Flex direction="column" {...props} />
-);
+export const VStack = (props: VStackProps) => {
+    const { align = 'start' } = props;
+
+    return <Flex direction="column" {...props} align={align} />;
+};
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { memo, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ButtonSize, ButtonVariant } from 'shared/ui/Button/Button';
+import { VStack } from 'shared/ui/Stack/VStack/VStack';
 import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { getSidebarItems } from '../../model/selectors/getSidebarItems';
@@ -44,9 +45,9 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
                 {collapsed ? '>' : '<'}
 
             </Button>
-            <div className={cls.items}>
+            <VStack gap="8" className={cls.items}>
                 {itemsList}
-            </div>
+            </VStack>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher short={collapsed} className={cls.lang} />
