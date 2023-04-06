bdfe82a04355cb427b49a83c9cd5b1f00689662f
slavislavi <slavislavi1986@gmail.com>
2023-04-06 15:45:07 +0300

    [main]: storybook-listbox

--- src/shared/ui/ListBox/ListBox.stories.tsx
+++ src/shared/ui/ListBox/ListBox.stories.tsx
@@ -12,4 +12,55 @@ export default {
 const Template: ComponentStory<typeof ListBox> = (args) => <ListBox {...args} />;
 
 export const Normal = Template.bind({});
-Normal.args = {};
+Normal.args = {
+    value: '123',
+    items: [
+        { content: 'Content number', value: '123' },
+        { content: 'Splash Adventure', value: '1232' },
+        { content: 'Suite candles', value: '669' },
+    ],
+};
+
+export const TopLeft = Template.bind({});
+TopLeft.args = {
+    direction: 'top left',
+    value: '123',
+    items: [
+        { content: 'Content number', value: '123' },
+        { content: 'Splash Adventure', value: '1232' },
+        { content: 'Suite candles', value: '669' },
+    ],
+};
+
+export const TopRight = Template.bind({});
+TopRight.args = {
+    direction: 'top right',
+    value: '123',
+    items: [
+        { content: 'Content number', value: '123' },
+        { content: 'Splash Adventure', value: '1232' },
+        { content: 'Suite candles', value: '669' },
+    ],
+};
+
+export const BottomLeft = Template.bind({});
+BottomLeft.args = {
+    direction: 'bottom left',
+    value: '123',
+    items: [
+        { content: 'Content number', value: '123' },
+        { content: 'Splash Adventure', value: '1232' },
+        { content: 'Suite candles', value: '669' },
+    ],
+};
+
+export const BottomRight = Template.bind({});
+BottomRight.args = {
+    direction: 'bottom right',
+    value: '123',
+    items: [
+        { content: 'Content number', value: '123' },
+        { content: 'Splash Adventure', value: '1232' },
+        { content: 'Suite candles', value: '669' },
+    ],
+};
