d95c234e5db599bb79be3f58851c1a71884b2314
slavislavi <slavislavi1986@gmail.com>
2023-04-09 11:34:21 +0300

    [main]: storybook-tabs

--- src/shared/ui/Tabs/Tabs.stories.tsx
+++ src/shared/ui/Tabs/Tabs.stories.tsx
@@ -1,5 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
 import { Tabs } from './Tabs';
 
 export default {
@@ -12,22 +14,40 @@ export default {
 
 const Template: ComponentStory<typeof Tabs> = (args) => <Tabs {...args} />;
 
+const testTabs = [
+    {
+        value: 'tab 1',
+        content: 'tab 1',
+    },
+    {
+        value: 'tab 2',
+        content: 'tab 2',
+    },
+    {
+        value: 'tab 3',
+        content: 'tab 3',
+    },
+];
+
 export const Normal = Template.bind({});
 Normal.args = {
-    tabs: [
-        {
-            value: 'tab 1',
-            content: 'tab 1',
-        },
-        {
-            value: 'tab 2',
-            content: 'tab 2',
-        },
-        {
-            value: 'tab 3',
-            content: 'tab 3',
-        },
-    ],
+    tabs: testTabs,
     value: 'tab 2',
     onTabClick: action('onTabClick'),
 };
+
+export const Dark = Template.bind({});
+Dark.args = {
+    tabs: testTabs,
+    value: 'tab 2',
+    onTabClick: action('onTabClick'),
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const Orange = Template.bind({});
+Orange.args = {
+    tabs: testTabs,
+    value: 'tab 2',
+    onTabClick: action('onTabClick'),
+};
+Dark.decorators = [ThemeDecorator(Theme.ORANGE)];
