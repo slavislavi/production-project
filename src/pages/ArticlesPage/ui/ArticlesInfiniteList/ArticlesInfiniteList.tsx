e295719b2b7389889328080748e131b732954cf1
slavislavi <slavislavi1986@gmail.com>
2023-04-10 18:09:28 +0300

    [main]: refactor-articlesPage

---
+++ src/pages/ArticlesPage/ui/ArticlesInfiniteList/ArticlesInfiniteList.tsx
@@ -0,0 +1,37 @@
+import { memo } from 'react';
+import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { ArticleList } from 'entities/Article';
+import { Text } from 'shared/ui/Text/Text';
+import { getArticles } from '../../model/slice/articlesPageSlice';
+import {
+    getArticlesPageError,
+    getArticlesPageIsLoading,
+    getArticlesPageView,
+} from '../../model/selectors/articlesPage';
+
+interface ArticlesInfiniteListProps {
+    className?: string;
+}
+
+export const ArticlesInfiniteList = memo((props: ArticlesInfiniteListProps) => {
+    const { className } = props;
+    const { t } = useTranslation();
+    const articles = useSelector(getArticles.selectAll);
+    const isLoading = useSelector(getArticlesPageIsLoading);
+    const view = useSelector(getArticlesPageView);
+    const error = useSelector(getArticlesPageError);
+
+    if (error) {
+        return <Text text={t('Ошибка при загрузке статей', { ns: 'errors' })} />;
+    }
+
+    return (
+        <ArticleList
+            isLoading={isLoading}
+            view={view}
+            articles={articles}
+            className={className}
+        />
+    );
+});
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,21 +1,15 @@
 import { memo, useCallback } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { useTranslation } from 'react-i18next';
-import { ArticleList } from 'entities/Article';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { DynamicReducerLoader, ReducersList } from 'shared/lib/components/DynamicReducerLoader/DynamicReducerLoader';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { Page } from 'widgets/Page/Page';
-import {
-    getArticlesPageError, getArticlesPageIsLoading,
-    getArticlesPageView,
-} from '../../model/selectors/articlesPage';
 import { fetchNextArticlesPage } from '../../model/services/fetchNextArticlesPage/fetchNextArticlesPage';
 import { initArticlesPage } from '../../model/services/initArticlesPage/initArticlesPage';
-import { articlesPageReducer, getArticles } from '../../model/slice/articlesPageSlice';
+import { articlesPageReducer } from '../../model/slice/articlesPageSlice';
 import { ArticlesPageFilters } from '../ArticlesPageFilters/ArticlesPageFilters';
+import { ArticlesInfiniteList } from '../ArticlesInfiniteList/ArticlesInfiniteList';
 import cls from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
@@ -28,12 +22,7 @@ const reducers: ReducersList = {
 
 export const ArticlesPage = memo((props: ArticlesPageProps) => {
     const { className } = props;
-    const { t } = useTranslation();
     const dispatch = useAppDispatch();
-    const articles = useSelector(getArticles.selectAll);
-    const isLoading = useSelector(getArticlesPageIsLoading);
-    const view = useSelector(getArticlesPageView);
-    const error = useSelector(getArticlesPageError);
     const [searchParams] = useSearchParams();
 
     const onLoadNextPart = useCallback(() => {
@@ -51,12 +40,7 @@ export const ArticlesPage = memo((props: ArticlesPageProps) => {
                 onScrollEnd={onLoadNextPart}
             >
                 <ArticlesPageFilters />
-                <ArticleList
-                    isLoading={isLoading}
-                    view={view}
-                    articles={articles}
-                    className={cls.list}
-                />
+                <ArticlesInfiniteList className={cls.list} />
             </Page>
         </DynamicReducerLoader>
     );
