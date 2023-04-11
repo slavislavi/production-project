7f2a113ee0f8057316d74e99c5e2ed27db1e9976
slavislavi <slavislavi1986@gmail.com>
2023-04-11 14:08:48 +0300

    [main]: ui-tests-editable-profile-card

--- src/features/EditableProfileCard/ui/EditableProfileCard/EditableProfileCard.test.tsx
+++ src/features/EditableProfileCard/ui/EditableProfileCard/EditableProfileCard.test.tsx
@@ -3,6 +3,7 @@ import userEvent from '@testing-library/user-event';
 import { Country } from 'entities/Country';
 import { Currency } from 'entities/Currency';
 import { Profile } from 'entities/Profile';
+import { $api } from 'shared/api/api';
 import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { profileReducer } from '../../model/slice/profileSlice';
 import { EditableProfileCard } from './EditableProfileCard';
@@ -39,5 +40,43 @@ describe('features/EditableProfileCard', () => {
         componentRender(<EditableProfileCard id="1" />, testOptions);
         await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
         expect(screen.getByTestId('EditableProfileCardHeader.EditButton')).toBeInTheDocument();
+    });
+
+    test('set new name, but on click cancel, previous form must be reset', async () => {
+        componentRender(<EditableProfileCard id="1" />, testOptions);
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
+
+        await userEvent.clear(screen.getByTestId('ProfileCard.firstName'));
+        await userEvent.clear(screen.getByTestId('ProfileCard.lastName'));
+
+        await userEvent.type(screen.getByTestId('ProfileCard.firstName'), 'Eddard');
+        await userEvent.type(screen.getByTestId('ProfileCard.lastName'), 'Stark');
+
+        expect(screen.getByTestId('ProfileCard.firstName')).toHaveValue('Eddard');
+        expect(screen.getByTestId('ProfileCard.lastName')).toHaveValue('Stark');
+
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.CancelButton'));
+
+        expect(screen.getByTestId('ProfileCard.firstName')).toHaveValue('John');
+        expect(screen.getByTestId('ProfileCard.lastName')).toHaveValue('Snow');
+    });
+
+    test('must throw error when set empty input', async () => {
+        componentRender(<EditableProfileCard id="1" />, testOptions);
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
+        await userEvent.clear(screen.getByTestId('ProfileCard.firstName'));
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.SaveButton'));
+
+        expect(screen.getByTestId('EditableProfileCard.Error.Paragraph')).toBeInTheDocument();
+    });
+
+    test('must throw error when set empty input', async () => {
+        const mockPUTrequest = jest.spyOn($api, 'put');
+        componentRender(<EditableProfileCard id="1" />, testOptions);
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
+        await userEvent.type(screen.getByTestId('ProfileCard.lastName'), 'Stark');
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.SaveButton'));
+
+        expect(mockPUTrequest).toHaveBeenCalled();
     });
 });
