018519234d5390fb62ae5a870c72ebc7a605d7ab
slavislavi <slavislavi1986@gmail.com>
2023-04-11 18:06:51 +0300

    [main]: fix-styles-stack

--- src/entities/Comment/ui/CommentCard/CommentCard.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -19,13 +19,17 @@ export const CommentCard = (props: CommentCardProps) => {
 
     if (isLoading) {
         return (
-            <div className={classNames(cls.commentCard, {}, [className, cls.loading])}>
+            <VStack
+                gap="8"
+                max
+                className={classNames(cls.commentCard, {}, [className, cls.loading])}
+            >
                 <div className={cls.header}>
                     <Skeleton width={30} height={30} border="50%" />
                     <Skeleton width={100} height={16} className={cls.username} />
                 </div>
                 <Skeleton width="100%" height={50} />
-            </div>
+            </VStack>
         );
     }
 
@@ -34,7 +38,11 @@ export const CommentCard = (props: CommentCardProps) => {
     }
 
     return (
-        <VStack gap="16" max className={classNames(cls.commentCard, {}, [className])}>
+        <VStack
+            gap="8"
+            max
+            className={classNames(cls.commentCard, {}, [className])}
+        >
             <AppLink to={`${RoutePath.profile}${comment.user.id}`} className={cls.header}>
                 {comment.user.avatar ? <Avatar size={30} src={comment.user.avatar} /> : null}
                 <Text className={cls.username} title={comment.user.username} />
