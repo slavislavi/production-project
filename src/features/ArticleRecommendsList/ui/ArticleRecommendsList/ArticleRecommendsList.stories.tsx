85f214951e558d8e9bf03086d5527bf0f540a987
slavislavi <slavislavi1986@gmail.com>
2023-04-12 13:31:53 +0300

    [main]: storybook-mock-addon

--- src/features/ArticleRecommendsList/ui/ArticleRecommendsList/ArticleRecommendsList.stories.tsx
+++ src/features/ArticleRecommendsList/ui/ArticleRecommendsList/ArticleRecommendsList.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import withMock from 'storybook-addon-mock';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { mockArticle } from 'entities/Article/mocks/data.mock';
 import { ArticleRecommendsList } from './ArticleRecommendsList';
 
 export default {
@@ -9,6 +11,7 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [withMock],
 } as ComponentMeta<typeof ArticleRecommendsList>;
 
 const Template: ComponentStory<typeof ArticleRecommendsList> = (args) => <ArticleRecommendsList {...args} />;
@@ -16,3 +19,17 @@ const Template: ComponentStory<typeof ArticleRecommendsList> = (args) => <Articl
 export const Normal = Template.bind({});
 Normal.args = {};
 Normal.decorators = [StoreDecorator({})];
+Normal.parameters = {
+    mockData: [
+        {
+            url: `${__API__}/articles?_limit=3`,
+            method: 'GET',
+            status: 200,
+            response: [
+                { ...mockArticle, id: '1' },
+                { ...mockArticle, id: '2' },
+                { ...mockArticle, id: '3' },
+            ],
+        },
+    ],
+};
