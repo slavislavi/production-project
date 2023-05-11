1b17aeab39b17564141eddbd6e5f5c876f823e56
slavislavi <slavislavi1986@gmail.com>
2023-05-11 16:46:24 +0300

    [main]: userApi

---
+++ src/entities/User/model/api/userApi.ts
@@ -0,0 +1,22 @@
+import { rtkApi } from '@/shared/api/rtkApi';
+import { JsonSettings } from '../types/jsonSettings';
+import { User } from '../types/user';
+
+interface SetJsonSettingsArg {
+    userId: string;
+    jsonSettings: JsonSettings;
+}
+
+const userApi = rtkApi.injectEndpoints({
+    endpoints: (build) => ({
+        setJsonSettings: build.mutation<User, SetJsonSettingsArg>({
+            query: ({ userId, jsonSettings }) => ({
+                url: `/users/${userId}`,
+                method: 'PATCH',
+                body: {
+                    jsonSettings,
+                },
+            }),
+        }),
+    }),
+});
