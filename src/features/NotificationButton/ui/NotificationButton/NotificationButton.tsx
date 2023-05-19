cb7edb0212c167066d18fced883fb674499ffc41
slavislavi <slavislavi1986@gmail.com>
2023-05-19 14:39:27 +0300

    [main]: notofocation-button-redesigned

--- src/features/NotificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/NotificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -1,12 +1,16 @@
 import { memo, useCallback, useState } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { Button, ButtonVariant } from '@/shared/ui/deprecated/Button';
-import { Icon } from '@/shared/ui/deprecated/Icon';
-import NotificationIcon from '@/shared/assets/icons/notification-20-20.svg';
+import { Button as ButtonDeprecated, ButtonVariant } from '@/shared/ui/deprecated/Button';
+import { Icon as IconDeprecated } from '@/shared/ui/deprecated/Icon';
+import NotificationIconDeprecated from '@/shared/assets/icons/notification-20-20.svg';
+import NotificationIcon from '@/shared/assets/icons/notification.svg';
 import { NotificationList } from '@/entities/Notification';
-import { Popover } from '@/shared/ui/deprecated/Popups';
+import { Popover as PopoverDeprecated } from '@/shared/ui/deprecated/Popups';
 import { Drawer } from '@/shared/ui/deprecated/Drawer';
 import { useDevice } from '@/shared/lib/hooks/useDevice/useDevice';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { Icon } from '@/shared/ui/redesigned/Icon';
+import { Popover } from '@/shared/ui/redesigned/Popups';
 import cls from './NotificationButton.module.scss';
 
 interface NotificationButtonProps {
@@ -27,9 +31,20 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
     }, []);
 
     const trigger = (
-        <Button onClick={onOpenDrawer} variant={ButtonVariant.CLEAR}>
-            <Icon Svg={NotificationIcon} inverted />
-        </Button>
+        <ToggleFeatures
+            feature="isAppRedesigned"
+            on={
+                <Icon Svg={NotificationIcon} clickable onClick={onOpenDrawer} />
+            }
+            off={(
+                <ButtonDeprecated
+                    onClick={onOpenDrawer}
+                    variant={ButtonVariant.CLEAR}
+                >
+                    <IconDeprecated Svg={NotificationIconDeprecated} inverted />
+                </ButtonDeprecated>
+            )}
+        />
     );
 
     if (isMobile) {
@@ -44,12 +59,26 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
     }
 
     return (
-        <Popover
-            className={classNames(cls.notificationButton, {}, [className])}
-            direction="bottom left"
-            trigger={trigger}
-        >
-            <NotificationList className={cls.notifications} />
-        </Popover>
+        <ToggleFeatures
+            feature="isAppRedesigned"
+            on={(
+                <Popover
+                    className={classNames('', {}, [className])}
+                    direction="bottom left"
+                    trigger={trigger}
+                >
+                    <NotificationList className={cls.notifications} />
+                </Popover>
+            )}
+            off={(
+                <PopoverDeprecated
+                    className={classNames('', {}, [className])}
+                    direction="bottom left"
+                    trigger={trigger}
+                >
+                    <NotificationList className={cls.notifications} />
+                </PopoverDeprecated>
+            )}
+        />
     );
 });
