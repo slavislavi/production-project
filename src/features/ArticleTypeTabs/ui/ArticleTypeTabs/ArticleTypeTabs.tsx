a37d38ce7bad61d64c0f922617e36920af491338
slavislavi <slavislavi1986@gmail.com>
2023-05-19 19:06:13 +0300

    [main]: redesign-article-type-tabs

--- src/features/ArticleTypeTabs/ui/ArticleTypeTabs/ArticleTypeTabs.tsx
+++ src/features/ArticleTypeTabs/ui/ArticleTypeTabs/ArticleTypeTabs.tsx
@@ -1,8 +1,10 @@
 import { memo, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { TabItem, Tabs } from '@/shared/ui/deprecated/Tabs';
+import { TabItem, Tabs as TabsDeprecated } from '@/shared/ui/deprecated/Tabs';
+import { Tabs } from '@/shared/ui/redesigned/Tabs';
 import { ArticleType } from '@/entities/Article';
+import { ToggleFeatures } from '@/shared/lib/features';
 import cls from './ArticleTypeTabs.module.scss';
 
 interface ArticleTypeTabsProps {
@@ -26,11 +28,25 @@ export const ArticleTypeTabs = memo((props: ArticleTypeTabsProps) => {
     }, [onChangeType]);
 
     return (
-        <Tabs
-            tabs={typeTabs}
-            value={value}
-            onTabClick={onTabClick}
-            className={classNames(cls.articleTypeTabs, {}, [className])}
+        <ToggleFeatures
+            feature="isAppRedesigned"
+            on={(
+                <Tabs
+                    direction="column"
+                    tabs={typeTabs}
+                    value={value}
+                    onTabClick={onTabClick}
+                    className={classNames('', {}, [className])}
+                />
+            )}
+            off={(
+                <TabsDeprecated
+                    tabs={typeTabs}
+                    value={value}
+                    onTabClick={onTabClick}
+                    className={classNames(cls.articleTypeTabs, {}, [className])}
+                />
+            )}
         />
     );
 });
