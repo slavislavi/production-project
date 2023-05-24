c2484c1fbc8647ccf157e18860b8de915860835f
slavislavi <slavislavi1986@gmail.com>
2023-05-24 15:27:38 +0300

    [main]: redesign-notification-list

--- src/entities/Notification/ui/NotificationList/NotificationList.tsx
+++ src/entities/Notification/ui/NotificationList/NotificationList.tsx
@@ -2,8 +2,10 @@ import { memo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { VStack } from '@/shared/ui/redesigned/Stack';
 import { Skeleton as SkeletonDeprecated } from '@/shared/ui/deprecated/Skeleton';
+import { Skeleton as SkeletonRedesigned } from '@/shared/ui/redesigned/Skeleton';
 import { NotificationItem } from '../NotificationItem/NotificationItem';
 import { useNotifications } from '../../api/notificationApi';
+import { toggleFeatures } from '@/shared/lib/features';
 import cls from './NotificationList.module.scss';
 
 interface NotificationListProps {
@@ -16,6 +18,12 @@ export const NotificationList = memo((props: NotificationListProps) => {
         pollingInterval: 10000,
     });
 
+    const Skeleton = toggleFeatures({
+        name: 'isAppRedesigned',
+        on: () => SkeletonRedesigned,
+        off: () => SkeletonDeprecated,
+    });
+
     if (isLoading) {
         return (
             <VStack
@@ -23,9 +31,9 @@ export const NotificationList = memo((props: NotificationListProps) => {
                 max
                 className={classNames(cls.notificationList, {}, [className])}
             >
-                <SkeletonDeprecated width="100%" border="8px" height="80px" />
-                <SkeletonDeprecated width="100%" border="8px" height="80px" />
-                <SkeletonDeprecated width="100%" border="8px" height="80px" />
+                <Skeleton width="100%" border="8px" height="80px" />
+                <Skeleton width="100%" border="8px" height="80px" />
+                <Skeleton width="100%" border="8px" height="80px" />
             </VStack>
         );
     }
