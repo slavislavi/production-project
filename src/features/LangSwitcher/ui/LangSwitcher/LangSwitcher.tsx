d68cf39e3aeb749a16257a9b0927870f689f8598
slavislavi <slavislavi1986@gmail.com>
2023-05-25 13:20:09 +0300

    [main]: fix-language-switcher

--- src/features/LangSwitcher/ui/LangSwitcher/LangSwitcher.tsx
+++ src/features/LangSwitcher/ui/LangSwitcher/LangSwitcher.tsx
@@ -19,7 +19,7 @@ export const LangSwitcher = memo(({ className, short }: LangSwitcherProps) => {
         <ToggleFeatures
             feature="isAppRedesigned"
             on={(
-                <Button variant="clear">
+                <Button variant="clear" onClick={toggle}>
                     {t(short ? 'Короткий язык' : 'Язык')}
                 </Button>
             )}
