5bb73fe2c75c855cd9a9beea4bd4ca652dad6fac
slavislavi <slavislavi1986@gmail.com>
2023-05-29 21:34:32 +0300

    [main]: minor-fix-button-ui

--- src/shared/ui/redesigned/Button/Button.tsx
+++ src/shared/ui/redesigned/Button/Button.tsx
@@ -3,7 +3,7 @@ import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Button.module.scss';
 
 export type ButtonVariant = 'clear' | 'outlined' | 'filled';
-
+export type ButtonColor = 'normal' | 'success' | 'error';
 export type ButtonSize = 'm' | 'l' | 'xl';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -36,6 +36,10 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
      */
     fullWidth?: boolean;
     /**
+     * Цвет кнопки в зависимости от семантики
+     */
+    color?: ButtonColor;
+    /**
      * Добавление иконки svg слева или справа инпута
      */
     addonLeft?: ReactNode;
@@ -52,6 +56,7 @@ export const Button = memo(({
     fullWidth,
     addonLeft,
     addonRight,
+    color = 'normal',
     ...otherProps
 }: ButtonProps) => {
     const mods: Mods = {
@@ -68,6 +73,7 @@ export const Button = memo(({
                 className,
                 cls[variant],
                 cls[size],
+                cls[color],
             ])}
             disabled={disabled}
             {...otherProps}
