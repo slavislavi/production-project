ea817c5117ff6438a9f319fd3df3d011dafdb5da
slavislavi <slavislavi1986@gmail.com>
2023-05-29 21:13:28 +0300

    [main]: article-recommends-list-redesign

--- src/features/ArticleRecommendsList/ui/ArticleRecommendsList/ArticleRecommendsList.tsx
+++ src/features/ArticleRecommendsList/ui/ArticleRecommendsList/ArticleRecommendsList.tsx
@@ -2,9 +2,11 @@ import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ArticleList } from '@/entities/Article';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { Text, TextSize } from '@/shared/ui/deprecated/Text';
-import { VStack } from '@/shared/ui/deprecated/Stack';
+import { Text as TextDeprecated, TextSize } from '@/shared/ui/deprecated/Text';
+import { Text } from '@/shared/ui/redesigned/Text';
+import { VStack } from '@/shared/ui/redesigned/Stack';
 import { useArticleRecommendsList } from '../../api/articleRecommendsApi';
+import { ToggleFeatures } from '@/shared/lib/features';
 
 interface ArticleRecommendsListProps {
     className?: string;
@@ -25,9 +27,15 @@ export const ArticleRecommendsList = memo((props: ArticleRecommendsListProps) =>
             gap="8"
             className={classNames('', {}, [className])}
         >
-            <Text
-                size={TextSize.L}
-                title={t('Рекомендуем', { ns: 'articleDetails' })}
+            <ToggleFeatures
+                feature="isAppRedesigned"
+                on={<Text size="l" title={t('Рекомендуем', { ns: 'articleDetails' })} />}
+                off={(
+                    <TextDeprecated
+                        size={TextSize.L}
+                        title={t('Рекомендуем')}
+                    />
+                )}
             />
             <ArticleList
                 articles={articles}
