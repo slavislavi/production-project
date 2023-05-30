2e57f3b46d722ad17e7c95401a6eeeaa730ddcdf
slavislavi <slavislavi1986@gmail.com>
2023-05-30 15:46:09 +0300

    [main]: login-form-redesigned

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -4,15 +4,20 @@ import { useSelector } from 'react-redux';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { DynamicReducerLoader, ReducersList } from '@/shared/lib/components/DynamicReducerLoader/DynamicReducerLoader';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { Button, ButtonVariant } from '@/shared/ui/deprecated/Button';
-import { Input } from '@/shared/ui/deprecated/Input';
-import { Text, TextVariant } from '@/shared/ui/deprecated/Text';
+import { Button as ButtonDeprecated, ButtonVariant } from '@/shared/ui/deprecated/Button';
+import { Button } from '@/shared/ui/redesigned/Button';
+import { Input as InputDeprecated } from '@/shared/ui/deprecated/Input';
+import { Input } from '@/shared/ui/redesigned/Input';
+import { Text as TextDeprecated, TextVariant } from '@/shared/ui/deprecated/Text';
+import { Text } from '@/shared/ui/redesigned/Text';
+import { VStack } from '@/shared/ui/redesigned/Stack';
 import { getLoginError } from '../../model/selectors/getLoginError/getLoginError';
 import { getLoginIsLoading } from '../../model/selectors/getLoginIsLoading/getLoginIsLoading';
 import { getLoginPassword } from '../../model/selectors/getLoginPassword/getLoginPassword';
 import { getLoginUsername } from '../../model/selectors/getLoginUsername/getLoginUsername';
 import { loginByUsername } from '../../model/services/loginByUsername/loginByUsername';
 import { loginActions, loginReducer } from '../../model/slice/loginSlice';
+import { ToggleFeatures } from '@/shared/lib/features';
 import cls from './LoginForm.module.scss';
 
 interface LoginFormProps {
@@ -48,33 +53,80 @@ export const LoginForm = memo(({ className, onSuccess }: LoginFormProps) => {
     }, [dispatch, onSuccess, password, username]);
 
     return (
-        <DynamicReducerLoader reducers={initialReducers}>
-            <div className={classNames(cls.loginForm, {}, [className])}>
-                <Text title={t('Авторизация')} />
-                {error && <Text text={t('Неверный логин или пароль')} variant={TextVariant.ERROR} />}
-                <Input
-                    autoFocus
-                    className={cls.input}
-                    placeholder={t('Имя')}
-                    onChange={onChangeUsername}
-                    value={username}
-                />
-                <Input
-                    className={cls.input}
-                    placeholder={t('Пароль')}
-                    onChange={onChangePassword}
-                    value={password}
-                />
-                <Button
-                    variant={ButtonVariant.OUTLINED}
-                    className={cls.loginBtn}
-                    onClick={onLoginClick}
-                    disabled={isLoading}
-                >
-                    {t('Войти')}
-                </Button>
-            </div>
+        <DynamicReducerLoader reducers={initialReducers} removeAfterUnmount>
+            <ToggleFeatures
+                feature="isAppRedesigned"
+                on={(
+                    <VStack
+                        gap="16"
+                        className={classNames(cls.loginForm, {}, [className])}
+                    >
+                        <Text title={t('Авторизации')} />
+                        {error && (
+                            <Text
+                                text={t('Неверный логин или пароль')}
+                                variant="error"
+                            />
+                        )}
+                        <Input
+                            autofocus
+                            type="text"
+                            className={cls.input}
+                            placeholder={t('Логин')}
+                            onChange={onChangeUsername}
+                            value={username}
+                        />
+                        <Input
+                            type="text"
+                            className={cls.input}
+                            placeholder={t('Пароль')}
+                            onChange={onChangePassword}
+                            value={password}
+                        />
+                        <Button
+                            className={cls.loginBtn}
+                            onClick={onLoginClick}
+                            disabled={isLoading}
+                        >
+                            {t('Войти')}
+                        </Button>
+                    </VStack>
+                )}
+                off={(
+                    <div className={classNames(cls.loginForm, {}, [className])}>
+                        <TextDeprecated title={t('Авторизации')} />
+                        {error && (
+                            <TextDeprecated
+                                text={t('Неверный логин или пароль')}
+                                variant={TextVariant.ERROR}
+                            />
+                        )}
+                        <InputDeprecated
+                            autoFocus
+                            type="text"
+                            className={cls.input}
+                            placeholder={t('Логин')}
+                            onChange={onChangeUsername}
+                            value={username}
+                        />
+                        <InputDeprecated
+                            type="text"
+                            className={cls.input}
+                            placeholder={t('Пароль')}
+                            onChange={onChangePassword}
+                            value={password}
+                        />
+                        <ButtonDeprecated
+                            variant={ButtonVariant.OUTLINED}
+                            className={cls.loginBtn}
+                            onClick={onLoginClick}
+                            disabled={isLoading}
+                        >
+                            {t('Войти')}
+                        </ButtonDeprecated>
+                    </div>
+                )}
+            />
         </DynamicReducerLoader>
-
     );
 });
